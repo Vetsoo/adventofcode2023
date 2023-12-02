f903cc46de9ca512be7b82c011599d2ac1a04da5
Jente Vets <jentevets@gmail.com>
2023-12-02 11:03:20 +0100

    day 2 part 1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
     static async Task Main(string[] args)
     {
         Console.WriteLine("Advent of code 2023!");
-        await Day1();
+        await Day2();
     }
 
     static async Task Day1()
@@ -41,7 +41,8 @@ class Program
 
         var listOfCalibrationValues = new List<int>();
 
-        await foreach (var line in inputText){
+        await foreach (var line in inputText)
+        {
             var firstIndexes = setOfValidationValues.Select(x => new KeyValuePair<int, int>(line.IndexOf(x.Key, 0), x.Value));
             var lastIndexes = setOfValidationValues.Select(x => new KeyValuePair<int, int>(line.LastIndexOf(x.Key, line.Length - 1), x.Value));
 
@@ -52,6 +53,46 @@ class Program
         }
 
         var sumofNewValues = listOfCalibrationValues.Sum();
-        Console.WriteLine($"Sum of new calibration values: {sumofNewValues}");   
+        Console.WriteLine($"Sum of new calibration values: {sumofNewValues}");
+    }
+
+    static async Task Day2()
+    {
+        Console.WriteLine("Welcome to the DAY 2!");
+        Console.WriteLine("");
+        Console.WriteLine("Started reading input...");
+        var games = File.ReadLinesAsync(@"input/day2.txt");
+        var cubeColorAndAmounts = new Dictionary<string, int>{
+            {"blue", 14},
+            {"red", 12},
+            {"green", 13}
+        };
+        var gameNumber = 0;
+        var possibleGames = new List<int>();
+        await foreach (var game in games)
+        {
+            gameNumber++;
+            var strippedString = game[(game.IndexOf(':') + 1)..].TrimStart();
+            var rounds = strippedString.Split("; ");
+            var isPossible = true;
+            foreach (var round in rounds)
+            {
+                var dicesPerRound = round.Split(", ");
+                foreach (var dice in dicesPerRound)
+                {
+                    var amountAndColor = dice.Split(' ');
+                    isPossible = cubeColorAndAmounts.First(x => x.Key == amountAndColor[1]).Value >= int.Parse(amountAndColor[0]);
+                    if (!isPossible)
+                        break;
+                }
+                if (!isPossible)
+                    break;
+            }
+            if (isPossible)
+                possibleGames.Add(gameNumber);
+        }
+
+        var sumOfPossibleGames = possibleGames.Sum();
+        Console.WriteLine($"Sum of possible games: {sumOfPossibleGames}");
     }
 }
